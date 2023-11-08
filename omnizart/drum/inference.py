180d96ae9735bef4818332afe458b51f7b1c7842
demoskalifi <143889431+demoskalifi@users.noreply.github.com>
2023-11-08 12:36:20 +0200

    Update inference.py - more instruments

--- omnizart/drum/inference.py
+++ omnizart/drum/inference.py
@@ -2,35 +2,97 @@ import pretty_midi
 import numpy as np
 from scipy.signal import find_peaks
 
+# Define the standard MIDI note numbers for the given drum components
+MIDI_PITCHES = {
+    'Kick': 36,
+    'Snare': 38,
+    'Closed_Hat': 42,
+    'Open_Hat': 46,
+    'Low_Tom': 45,
+    'Rim': 37,
+    'Clap': 39,
+}
 
-def get_3inst_ary(inst_13_ary_in):
-    inst_3_ary_out = np.zeros_like(inst_13_ary_in)[:, :3]
+# Define the indices of the instruments in the model's output
+MODEL_OUTPUT_INDICES = {
+    'Kick': 0,
+    'Snare': 1,
+    'Closed_Hat': 4,
+    'Open_Hat': 6,
+    'Low_Tom': 7,
+    'Rim': 2,
+    'Clap': 3,
+}
 
-    inst_3_ary_out[:, 0] = inst_13_ary_in[:, 0]
-    inst_3_ary_out[:, 1] = inst_13_ary_in[:, 1]
-    inst_3_ary_out[:, 2] = np.max([inst_13_ary_in[:, 4], inst_13_ary_in[:, 5], inst_13_ary_in[:, 6]], axis=0)
-    return inst_3_ary_out
+def filter_activations(activations_dict, pred, m_beat_arr):
+    LOW_SOUNDS = ['Kick', 'Low_Tom']
+    HIGH_SOUNDS = ['Snare', 'Closed_Hat', 'Open_Hat', 'Rim', 'Clap', 'Cowbell']
+
+    # This dictionary will hold the filtered activations
+    filtered_activations = {instrument: [] for instrument in activations_dict}
+
+    # We will iterate over each beat time
+    for beat_index, beat_time in enumerate(m_beat_arr):
+        # Gather activations for this specific beat
+        beat_activations = [(instrument, time_index) for instrument, times in activations_dict.items() for time_index in times if m_beat_arr[time_index] == beat_time]
+        
+        # Separate low and high sound activations
+        low_activations = [act for act in beat_activations if act[0] in LOW_SOUNDS]
+        high_activations = [act for act in beat_activations if act[0] in HIGH_SOUNDS]
+        
+        # Sort them by the prediction value, highest first
+        low_activations.sort(key=lambda x: pred[x[1], MODEL_OUTPUT_INDICES[x[0]]], reverse=True)
+        high_activations.sort(key=lambda x: pred[x[1], MODEL_OUTPUT_INDICES[x[0]]], reverse=True)
+        
+        # Select up to 1 low and 2 high activations
+        selected_low = low_activations[:1]
+        selected_high = high_activations[:2]
+        
+        # Add the selected activations to the filtered list
+        for instrument, time_index in selected_low + selected_high:
+            if time_index not in filtered_activations[instrument]:
+                filtered_activations[instrument].append(time_index)
+
+    # Sort the filtered activations for each instrument
+    for instrument in filtered_activations:
+        filtered_activations[instrument].sort()
+
+    return filtered_activations
 
 
-def inference(pred, m_beat_arr, bass_drum_th=0.85, snare_th=1.2, hihat_th=0.17):
-    insts = get_3inst_ary(pred)
+def inference(pred, m_beat_arr, bass_drum_th, snare_th, hihat_th, default_th=0.95):
+    midi_file = pretty_midi.PrettyMIDI()
+    drum_inst = pretty_midi.Instrument(program=0, is_drum=True, name="Drum Set")
 
-    norm = lambda x: (x - np.mean(x)) / np.std(x)
-    bass_drum_act, _ = find_peaks(norm(insts[:, 0]), height=bass_drum_th, distance=1)
-    snare_act, _ = find_peaks(norm(insts[:, 1]), height=snare_th, distance=1)
-    hihat_act, _ = find_peaks(norm(insts[:, 2]), height=hihat_th, distance=1)
+    hihat_th -= 0.05 
+    thresholds = {
+        'Kick': bass_drum_th,
+        'Snare': snare_th,
+        'Closed_Hat': hihat_th,
+        'Open_Hat': default_th,
+        'Low_Tom': default_th,
+        'Rim': default_th,
+        'Clap': default_th,
+    }
 
-    drum_inst = pretty_midi.Instrument(program=1, is_drum=True, name="drums")
+    activations_dict = {}
+    for instrument, index in MODEL_OUTPUT_INDICES.items():
+        norm_instrument = (pred[:, index] - np.mean(pred[:, index])) / np.std(pred[:, index])
+        activations, _ = find_peaks(norm_instrument, height=thresholds[instrument])
+        activations_dict[instrument] = activations
 
-    def register_notes(act_list, pitch):
-        for onset in m_beat_arr[act_list]:
-            note = pretty_midi.Note(velocity=100, pitch=pitch, start=onset, end=onset + 0.05)
+    filtered_activations = filter_activations(activations_dict, pred, m_beat_arr)
+
+    for instrument, times in filtered_activations.items():
+        for time in times:
+            beat_time = m_beat_arr[time]
+            note = pretty_midi.Note(
+                velocity=100,
+                pitch=MIDI_PITCHES[instrument],
+                start=beat_time,
+                end=beat_time + 0.1
+            )
             drum_inst.notes.append(note)
 
-    register_notes(bass_drum_act, pitch=35)
-    register_notes(snare_act, pitch=38)
-    register_notes(hihat_act, pitch=42)
-
-    midi = pretty_midi.PrettyMIDI(initial_tempo=120)
-    midi.instruments.append(drum_inst)
-    return midi
+    midi_file.instruments.append(drum_inst)
+    return midi_file
